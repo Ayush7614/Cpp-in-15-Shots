0a42ed617a7e50f9b6b7952af6591d995ea207dd
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-07-18 18:41:53 +0530

    Create Day3 code.cpp

---
+++ Shot 3/Day3 code.cpp
@@ -0,0 +1,105 @@
+This is the code that i have written in my text editor for suggestion of text editor you can use Dev C++ is best one
+
+
+
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+	int myNum = 15;
+	myNum =15;
+	cout<<myNum;
+	return 0;
+}
+*/
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+	int myNum = 15;
+	myNum = 10;
+	cout<<myNum;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int myAge = 20;
+	cout<<" I am " <<myAge <<" years old ";
+	return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 10;
+	int y = 5;
+	int sum = x + y;
+	cout<<sum;
+	return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 6, y = 6, z = 9;
+	cout<<x+y+z;
+	return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int minutesPerHour = 60;
+	int m = 60;
+	cout<< minutesPerHour<<"\n";
+	cout<< m;
+	return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	const int minPerHour = 60;
+	const  float PI = 3.14;
+	cout<<minPerHour<<"\n";
+	cout<<PI;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"Type a number:";
+	cin>>x;
+	cout<<"Your number is: " <<x;
+	return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int name;
+	cout<< "Type a name:";
+	cin>>name;
+	cout<<"Your name is:"<<name;
+	return 0;
+}
+*/
