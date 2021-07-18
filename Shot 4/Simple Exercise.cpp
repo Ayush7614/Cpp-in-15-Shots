1940f93398026d16d3d89313edc17e7c7a19cdb2
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-07-18 19:45:48 +0530

    Create Simple Exercise.cpp

---
+++ Shot 4/Simple Exercise.cpp
@@ -0,0 +1,16 @@
+/* Make a simple calculator that gives sum*/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int x, y;
+  int sum;
+  cout<<"Type a number:";
+  cin>>x;
+  cout<<"Type another number:";
+  cin>>y;
+  sum = x+y;
+  cout<<"Sum is: "<<sum;
+  return 0;
+}
+  
