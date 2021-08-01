f60e3688b9681735102baeca5b7b705eaf6cd431
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-08-01 11:10:48 +0530

    Update Data Types and Operators.cpp

--- Shot 5/Data Types and Operators.cpp
+++ Shot 5/Data Types and Operators.cpp
@@ -1,3 +1,82 @@
-C++ Data Types
+/* C++ Data Types
 As explained in the Variables chapter, a variable in C++ must be a specified data type:
+*/
 
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  // Creating variables
+  int myNum = 5;               // Integer (whole number)
+  float myFloatNum = 5.99;     // Floating point number
+  double myDoubleNum = 9.98;   // Floating point number
+  char myLetter = 'D';         // Character
+  bool myBoolean = true;       // Boolean
+  string myString = "Hello";   // String
+   
+  // Print variable values
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+ 
+  return 0;
+}
+*/
+
+Complete Program of all the data types 
+/*
+
+/* Number, Boolean,Characters, String*/
+#include<iostream>
+using namespace std;
+int main(){
+	int myNum = 5;
+	float myFloatNum = 5.5;
+	double myDoubleNum = 99.9;
+	char myLetter = 'A';
+	bool myBoolean = true;
+	string myString = "Hello";
+	
+	//Print Variables values
+	
+	cout << "int:" <<myNum << "\n";
+	cout << "float:" <<myFloatNum << "\n";
+	cout << "double:" <<myDoubleNum << "\n";
+	cout << "char:" <<myLetter << "\n";
+	cout << "bool:" <<myBoolean << "\n";
+	cout << "string:" <<myString << "\n";
+	
+	return 0;
+}*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int myNum = 6;
+	cout << "int:" <<myNum;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main (){
+	float myNum = 8.77;
+	cout<<myNum;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main (){
+	string myText = "Ayush Kumar";
+	cout<<myText;
+	return 0;
+}
+*/
