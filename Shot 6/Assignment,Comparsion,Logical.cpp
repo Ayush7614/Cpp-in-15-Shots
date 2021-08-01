76413af6b10c8d408d3908e826c8065d4b9d85b7
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-08-01 12:37:43 +0530

    Create Assignment,Comparsion,Logical.cpp

---
+++ Shot 6/Assignment,Comparsion,Logical.cpp
@@ -0,0 +1,38 @@
+/* 
+The addition assignment operator (+=) adds a value to a variable:
+*/
+
+/* Addition
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  x += 5;
+  cout << x;
+  return 0;
+}
+*/
+
+/* Subtraction
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  x -= 3;
+  cout << x;
+  return 0;
+}
+*/
+
+/*Comparison Operators
+Comparison operators are used to compare two values.
+
+Note: The return value of a comparison is either true (1) or false (0).
+
+In the following example, we use the greater than operator (>) to find out if 5 is greater than 3:
+
+*/
+
+
