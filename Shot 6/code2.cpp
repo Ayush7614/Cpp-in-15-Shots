947017a01f71d9fa429f5b4cf5471230f84a5db4
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-08-29 11:01:06 +0530

    Create code2.cpp

---
+++ Shot 6/code2.cpp
@@ -0,0 +1,116 @@
+/* Aritematic
+#include<iostream>
+using namespace std;
+int main(){
+	int x= 100+50;
+	cout<<x;
+	return 0;
+}
+*/
+/*
+Arithematic
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 10+5;
+	int y = 10+9;
+	int z = 10+10;
+	cout<<x<<"\n";
+	cout<<y<<"\n";
+	cout<<z;
+	return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 5;
+	++x;
+	cout<<x;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x = 10;
+	x += 5;
+	cout << x;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x = 10;
+	x -= 5;
+	cout << x;
+	return 0;
+}
+*/
+
+/* Strings*/
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string greeting = "Hello";
+	cout<<greeting;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string firstname = "John";
+	string lastname = "Doe";
+	string fullname = firstname+lastname;
+	cout<<fullname;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string firstname = "Ayush";
+	string lastname = "Kumar";
+	string fullname = firstname + " " +lastname;
+	cout<<fullname;
+	return 0;
+}
+*/
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string firstname = "Ayush";
+	string lastname = "Kumar";
+	string fullname = firstname.append(lastname);
+	cout << fullname;
+	return 0;
+}
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	int x = 10;
+	int y = 10;
+	int z = x + y;
+	cout << z;
+	return 0;
+}
